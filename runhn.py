97eaf6a623963b5c1a24cb8ccc9318a9dfb5497f
mmarceli <marcelimadej3@gmail.com>
2024-04-29 19:05:17 +0200

    Wrapper

--- runhn.py
+++ runhn.py
@@ -1,8 +1,12 @@
 import pandas as pd
 import scrapy
+from scrapy import CrawlerProcess
 
 df = pd.read_csv("scrapy/quotes-js-project/test.csv")
 tuple_list = list(zip(df.phrase, df.href))
 for phrase, href in tuple_list:
     print(phrase, href)
- 
+
+process = CrawlerProcess(get_project_settings())
+process.crawl(SearchSpider, topics_with_hrefs = topics_with_hrefs)
+process.start() 
